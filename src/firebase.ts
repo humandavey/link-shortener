8fed3b773a6b9d5e4281dd2cc5d31b77256c5b76
humandavey <davey.adams.three@gmail.com>
2025-08-10 19:19:15 -0500

    ability to create short links

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,16 +5,17 @@ import {
   getFirestore,
   doc,
   getDoc,
+  setDoc,
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -34,4 +35,15 @@ export const getURL = async (short: string): Promise<string> => {
   const docRef = doc(db, "links", short)
   const docSnap = await getDoc(docRef)
   return docSnap.data()!["url"]
+}
+
+export const createShort = async(url: string, short: string) => {
+  console.log({
+    name: short,
+    url: url,
+  })
+  await setDoc(doc(db, "links", short), {
+    name: short,
+    url: url,
+  })
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
+import { createShort } from '../firebase'
+
 export default () => {
+  const [url, setURL] = useState('')
+  const [short, setShort] = useState('')
+
   return (
     <>
-      <h1>Link Shortener</h1>
+      <div className='container'>
+        <input type='text' placeholder='Enter URL' className='url-field' onChange={(e) => setURL(e.target.value)}/>
+        <input type='text' placeholder='Enter short url' className='short-field' onChange={(e) => setShort(e.target.value)}/>
+
+        <div className='buttons'>
+          <Button className='go-btn' onClick={() => createShort(url, short)}>GO</Button>
+        </div>
+      </div>
     </>
   )
 }
